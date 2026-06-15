0461688d3e1fb774ada380731bb405e78ca13f7f
TUHIN RAKSHIT <147582062+TUHIN88-hub@users.noreply.github.com>
2026-06-15 11:02:24 +0530

    Create solution.cpp

---
+++ Day-13/Problem-01-Helpful-Maths/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<char> nums;
+
+    for (char ch : s) {
+        if (ch != '+') {
+            nums.push_back(ch);
+        }
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+        if (i != nums.size() - 1)
+            cout << "+";
+    }
+
+    return 0;
+}
