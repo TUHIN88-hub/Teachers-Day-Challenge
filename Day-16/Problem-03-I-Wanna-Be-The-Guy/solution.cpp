3fcebd41c8e1be37061a644a7801782f9c415376
TUHIN RAKSHIT <147582062+TUHIN88-hub@users.noreply.github.com>
2026-06-18 09:58:08 +0530

    Create solution.cpp

---
+++ Day-16/Problem-03-I-Wanna-Be-The-Guy/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<int> levels;
+
+    int p, x;
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        cin >> x;
+        levels.insert(x);
+    }
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> x;
+        levels.insert(x);
+    }
+
+    if (levels.size() == n)
+        cout << "I become the guy.";
+    else
+        cout << "Oh, my keyboard!";
+
+    return 0;
+}
