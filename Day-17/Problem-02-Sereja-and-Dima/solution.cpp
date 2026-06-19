6506917ed64d868198d906f227e311f4fdc43820
TUHIN RAKSHIT <147582062+TUHIN88-hub@users.noreply.github.com>
2026-06-19 10:06:31 +0530

    Create solution.cpp

---
+++ Day-17/Problem-02-Sereja-and-Dima/solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> cards(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> cards[i];
+    }
+
+    int left = 0, right = n - 1;
+    int sereja = 0, dima = 0;
+    bool turn = true;
+
+    while (left <= right) {
+        int chosen;
+
+        if (cards[left] > cards[right]) {
+            chosen = cards[left];
+            left++;
+        } else {
+            chosen = cards[right];
+            right--;
+        }
+
+        if (turn) {
+            sereja += chosen;
+        } else {
+            dima += chosen;
+        }
+
+        turn = !turn;
+    }
+
+    cout << sereja << " " << dima;
+
+    return 0;
+}
